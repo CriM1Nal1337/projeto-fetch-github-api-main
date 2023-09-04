7de0470e31a40c06fb19fc42e41d768b1bb15a08
Elton Ferreira Borges <eltonfborges123@gmail.com>
2023-09-04 17:24:37 -0300

    fix-identação

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -1,7 +1,8 @@
 const screen = {
     userProfile: document.querySelector('.profile-data'),
     renderUser(user) {
-        this.userProfile.innerHTML = `<div class="info">
+        this.userProfile.innerHTML = 
+        `<div class="info">
             <img src= "${user.avatarUrl}" alt="Foto do perfil do usuário">
                 <div class="data">
                     <h1>${user.name ?? 'Não possui nome cadastrado 😥'}</h1>
@@ -9,24 +10,28 @@ const screen = {
                     <br>
                     <p><i class="fas fa-users"></i> Seguidores: (${user.followers ?? 'Não encontrado/Não tem seguidores'}) Seguindo: (${user.following ?? 'Não encontrado/Não segue ninguém'})</p>
                 </div>
-            </div>`
+        </div>`
 
         let repositoriesItens = ''
-        user.repositories.forEach(repo => repositoriesItens += `<li>
-        <div class="about">
-            <ul>
-                <li><i class="fas fa-utensils"></i> ${repo.forks ?? 'Não há forks'}</li>
-                <li><i class="fas fa-star fa-spin"  style="color: #faf200;"></i> ${repo.stargazers_count ?? 'Não há stars'
-                }</li>
-                <li><i class="fas fa-eye"></i> ${repo.watchers ?? 'Não contém "watchers"'}</li>
-                <li><i class="fas fa-laptop-code"></i> ${repo.language ?? 'Não há Linguagem'}</li>
-            </ul>
-        </div>
-        <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-    </li>`)
+        user.repositories.forEach(repo => repositoriesItens += 
+        `<li>
+            <div class="about">
+                <ul>
+                    <li><i class="fas fa-utensils"></i> ${repo.forks ?? 'Não há forks'}</li>
+                    <li><i class="fas fa-star fa-spin"  style="color: #faf200;"></i> ${repo.stargazers_count ?? 'Não há stars'
+                    }</li>
+                    <li><i class="fas fa-eye"></i> ${repo.watchers ?? 'Não contém "watchers"'}</li>
+                    <li><i class="fas fa-laptop-code"></i> ${repo.language ?? 'Não há Linguagem'}</li>
+                </ul>
+            </div>
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+        </li>`)
         if (user.repositories.length > 0) {
-            this.userProfile.innerHTML += `<div class="repositories section">
-            <h2>Repositórios</h2><ul>${repositoriesItens}</ul></div>`
+            this.userProfile.innerHTML += 
+            `<div class="repositories section">
+                <h2>Repositórios</h2>
+                <ul>${repositoriesItens}</ul>
+            </div>`
         }
         console.log(user);
         let eventsItens = ''
@@ -39,9 +44,9 @@ const screen = {
 
         if (user.events.length > 0) {
             this.userProfile.innerHTML += `<div class="events">
-                                            <h2>Eventos</h2>
-                                            <ul>${eventsItens}</ul>
-                                        </div>`
+                                                <h2>Eventos</h2>
+                                                <ul>${eventsItens}</ul>
+                                            </div>`
         }
 
 
